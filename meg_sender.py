e8de52a7a54881a62e2a9dce36bd8015c7261f3c
parkheonseung <gus0tmd@naver.com>
2021-12-03 11:19:34 +0900

    lsc

--- meg_sender.py
+++ meg_sender.py
@@ -2,5 +2,5 @@ import socket
 
 sender = socket.socket(family=socket.AF_INET,type = socket.SOCK_DGRAM)
 
-sender.sendto(str.encode('hello'),('192.168.16.11',7778))
+sender.sendto(str.encode('hello'),('192.168.16.21',2777))
 
