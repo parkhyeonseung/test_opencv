93bfd758c70be9cbe021aed979b5ce00a7e8cf43
parkhyeonseung <gus0tmd@gmail.com>
2021-12-09 16:18:03 +0900

    1

--- flask/server_flask.py
+++ flask/server_flask.py
@@ -1,20 +1,31 @@
 from flask import Flask
+from flask import request
+from flask import Response
+from flask import render_template
 import cv2
 from camera import gstreamer_pipeline
 
 app = Flask(__name__)
-@app.route("/")
-def helloworld():
-    str = "Hello World! park"
-    return str
+# cap = cv2.VideoCapture(gstreamer_pipeline(),cv2.CAP_GSTREAMER)
+cap = cv2.VideoCapture(0)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route("/video_feed")
+def video_feed():
+    return Response(stream_gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def stream_gen():
+    while True:
+        success, frame = cap.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('turtle.png', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n') 
+
 
 if __name__ == '__main__':
-    # cap = cv2.VideoCapture(gstreamer_pipeline(),cv2.CAP_GSTREAMER)
-    cap = cv2.VideoCapture(0)
-    while cap.isOpened():
-        ret,frame = cap.read()
-        cv2.imshow('web',frame)
-        cv2.waitKey(1)
-        pass
-
     app.run(host = '0.0.0.0',port = 8000)
