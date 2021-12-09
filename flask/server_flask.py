b793bc8aa8bcb0dd9feb020d19e703970e321be8
parkheonseung <gus0tmd@naver.com>
2021-12-09 15:47:43 +0900

    1

--- flask/server_flask.py
+++ flask/server_flask.py
@@ -1,8 +1,20 @@
 from flask import Flask
+import cv2
+from camera import gstreamer_pipeline
 
 app = Flask(__name__)
 @app.route("/")
 def helloworld():
     str = "Hello World! park"
     return str
-app.run(host = '0.0.0.0',port = 8000)
+
+if __name__ == '__main__':
+    # cap = cv2.VideoCapture(gstreamer_pipeline(),cv2.CAP_GSTREAMER)
+    cap = cv2.VideoCapture(0)
+    while cap.isOpened():
+        ret,frame = cap.read()
+        cv2.imshow('web',frame)
+        cv2.waitKey(1)
+        pass
+
+    app.run(host = '0.0.0.0',port = 8000)
